bc061399be0491975397478b1931a73aeb6ee48b
Bartlomiej Sokolowski <Bart.Sokolowski@grouptree.com>
2025-12-02 23:55:04 +0000

    DEV - post merge changes

--- BrowserFileUploader/obj/Debug/net9.0/BrowserFileUploader.AssemblyInfo.cs
+++ BrowserFileUploader/obj/Debug/net9.0/BrowserFileUploader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrowserFileUploader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293970c063f71769438d18b80818ba9828277c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrowserFileUploader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrowserFileUploader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

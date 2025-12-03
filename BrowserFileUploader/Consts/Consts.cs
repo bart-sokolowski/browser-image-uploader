583ba7c69a13080e6c878e26255876cb623dd9e7
Bartlomiej Sokolowski <Bart.Sokolowski@grouptree.com>
2025-12-03 08:51:29 +0000

    DEV - updated the default storage mode string

--- BrowserFileUploader/Consts/Consts.cs
+++ BrowserFileUploader/Consts/Consts.cs
@@ -3,7 +3,7 @@
     public class Consts
     {
         public const int MAX_DIMENSIONS = 1024;
-        public const string DEFAULT_STORAGE_MODE = "FileSystem";
+        public const string DEFAULT_STORAGE_MODE = "FILE_SYSTEM";
         public const string DEFAULT_FILE_STORAGE_UPLOAD_PATH = "uploads";
 
         public static readonly string[] ALLOWED_CONTENT_TYPES =
